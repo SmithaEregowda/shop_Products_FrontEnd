1fc55b79feb375bc9316dcff13787d4a4b188efa
SmithaEregowda <smithahe@gmail.com>
2022-12-19 08:40:01 +0530

    next config added

--- next.config.js
+++ next.config.js
@@ -1,5 +1,16 @@
-module.exports = {
-  env: {
-    SERVER: process.env.SERVER,
-  }
+module.exports = () =>  {
+  const rewrites = () => {
+    return [
+      {
+        source: "/",
+        destination: "https://shop-products-o20x2vsou-smithaeregowda.vercel.app/",
+      },
+    ];
+  };
+  return {
+    env: {
+      SERVER: process.env.SERVER,
+    },
+    rewrites,
+  };
 }
