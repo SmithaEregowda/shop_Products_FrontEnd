cdb50e4e8493513273bd41c8db5567d4f4846971
SmithaEregowda <smithahe@gmail.com>
2023-09-06 08:12:49 +0530

    layout issue fixed

--- components/Layout/Layout.js
+++ components/Layout/Layout.js
@@ -1,20 +1,11 @@
 import React, { useEffect, useState } from 'react'
 import { styled } from '@mui/material/styles';
-import Box from '@mui/material/Box';
 import Paper from '@mui/material/Paper';
-import Grid from '@mui/material/Grid';
 import UserData from './user';
 import { Typography } from '@mui/material';
 import { useSelector } from 'react-redux';
 import { PERMISSIONS } from './constant';
 import { useRouter } from 'next/router';
-const Item = styled(Paper)(({ theme }) => ({
-    // backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
-    // ...theme.typography.body2,
-    padding: theme.spacing(1),
-    textAlign: 'center',
-    color: theme.palette.text.secondary,
-}));
 const Layout = ({ children }) => {
     const userdata = useSelector(state => state.user);
     const [permissions, setPermission] = useState([])
@@ -30,31 +21,21 @@ const Layout = ({ children }) => {
     }, [userdata])
     return (
         <div className='layout'>
-            <Grid container spacing={0} className="materialGrid">
-                <Grid item xs={2}>
-                    <Item>
+            <div className="layoutwraaper">
                         <div onClick={()=>router.push('/')} className='title'>
                             Veggies
                         </div>
-                    </Item>
-                </Grid>
-                <Grid item xs={8}>
-                    <Item>
-                        <div className='layout-items'>
+                <div className='layout-items'>
                             {permissions && permissions.length > 0 && permissions.map(item => (
                                 <div key={item} onClick={()=>router.push(item)}>
                                     <Typography sx={{ minWidth: 100 }}>{item.toUpperCase()}</Typography>
                                 </div>
                             ))}
-                        </div>
-                    </Item>
-                </Grid>
-                <Grid item xs={2}>
-                    <Item>
-                        <UserData />
-                    </Item>
-                </Grid>
-            </Grid>
+                </div>
+                <div className='signup-items'>
+                    <UserData />
+                </div>
+            </div>
             <div>{children}</div>
         </div>
     )
